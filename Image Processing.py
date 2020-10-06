87e5ce7b00d1f00ec7d23619377d20c3dbc29c6a
wawawiwaha <72433132+wawawiwaha@users.noreply.github.com>
2020-10-06 17:33:58 +0700

    Add files via upload

---
+++ Image Processing.py
@@ -0,0 +1,29 @@
+# UJIAN AKHIR SEMESTER ( UAS )
+# WAWA WIWAHA - 171011402043
+# KECERDASAN BUATAN
+# IMAGE PROCESSING
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+from skimage.util import img_as_ubyte
+
+
+image = img_as_ubyte(data.camera())
+
+fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(10, 4))
+
+img0 = ax0.imshow(image, cmap=plt.cm.gray)
+ax0.set_title('Image')
+ax0.axis('off')
+fig.colorbar(img0, ax=ax0)
+
+img1 = ax1.imshow(entropy(image, disk(5)), cmap=plt.cm.jet)
+ax1.set_title('Entropy')
+ax1.axis('off')
+fig.colorbar(img1, ax=ax1)
+
+plt.show()
